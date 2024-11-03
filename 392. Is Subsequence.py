3f493850d3a4e5130fc9050158825f44b817fe9b
tanveer-99 <tanvir3512@gmail.com>
2024-11-03 13:23:02 +0000

    392

--- 392. Is Subsequence.py
+++ 392. Is Subsequence.py
@@ -14,3 +14,11 @@ class Solution:
             return False
 
 # the time complexity of this solution is O(n)
+
+# An observation:
+    # this solution takes 0 ms runtime, and if i solve this with a while loop, it takes 28 ms runtime. why?
+    # because of python's internal optimization, a for loop has fixed ranges and boundaries and a while loop wil iterate
+    # every time until it gets an edge case, so it will check for conditions every time
+    # so, the overhead becomes relatively greater for while loops than for loops.
+    # so, the extra runtime generates from being checking the conditions is every loop, while a for loop just knows that
+    # i have to execute for this certain period.
